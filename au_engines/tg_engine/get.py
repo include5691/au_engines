74edd6dbb6a0869a2f9cb19d7c6f46c61d08f16e
Andrey Pshenitsyn <include5691@gmail.com>
2025-01-31 19:13:40 +0500

    update[tg_engine/get]: implement getting session by user id instead of phone

--- au_engines/tg_engine/get.py
+++ au_engines/tg_engine/get.py
@@ -30,17 +30,20 @@ def get_telegram_channels(user_id: str | int) -> list[TelegramChannel] | None:
         logging.error(f"Exception: failed to get telegram channels: {e}")
         return None
 
-def get_telegram_client(phone: str) -> Client | None:
+def get_telegram_client(*args, id: int | None = None, phone: str | None = None) -> Client | None:
+    if args:
+        raise ValueError("get_telegram_client() takes keyword arguments only")
     storage_path = Path(__file__).parent / "sessions"
+    key = f"{id}{phone}".replace("None", "")
     if storage_path.exists():
-        session_path = storage_path / f"{phone}.session"
+        session_path = storage_path / f"{key}.session"
         if session_path.exists():
-            return Client(phone, api_id=TG_API_ID, api_hash=TG_API_HASH, phone_number=phone, workdir=storage_path)
+            return Client(key, api_id=TG_API_ID, api_hash=TG_API_HASH, phone_number=phone, workdir=storage_path)
     try:
         with requests.Session() as session:
             response = session.post(
                 url=os.getenv("CHC_TELEGRAM_SESSIONS_URL"),
-                json={"phone": phone},
+                json={"id": id, "phone": phone},
                 headers={"Authorization": totp.now()},
                 timeout=10,
             )
@@ -48,9 +51,9 @@ def get_telegram_client(phone: str) -> Client | None:
                 return None
             if not storage_path.exists():
                 storage_path.mkdir()
-            with open(storage_path / f"{phone}.session", "wb") as file:
+            with open(storage_path / f"{key}.session", "wb") as file:
                 file.write(response.content)
-            return Client(phone, api_id=TG_API_ID, api_hash=TG_API_HASH, phone_number=phone, workdir=storage_path)
+            return Client(key, api_id=TG_API_ID, api_hash=TG_API_HASH, phone_number=phone, workdir=storage_path)
     except RequestException as e:
         logging.error(f"RequestException: failed to get telegram client: {e}")
         return None
