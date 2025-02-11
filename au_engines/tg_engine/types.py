30f3606fc6c06dc891abd66472911123f0d56fa6
Andrey Pshenitsyn <include5691@gmail.com>
2025-02-11 17:41:36 +0500

    fix[tg_engine/types]: fix typing for channels without username

--- au_engines/tg_engine/types.py
+++ au_engines/tg_engine/types.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, model_validator
 
 class TelegramChannel(BaseModel):
     id: int
-    username: str
+    username: str | None
     phone: str
     daily_limit: int
     toggle_mail: bool
