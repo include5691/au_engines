66e1cd7191c31c4e11fd8f74b024d385bcd90125
Andrey Pshenitsyn <include5691@gmail.com>
2025-12-13 20:54:01 +0500

    refactor(ma-engine/schemas): move in credentials schemas

--- au_engines/instances_base/mixins.py
+++ au_engines/instances_base/mixins.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, AliasChoices
 
 
 class PhoneNumberMixin(BaseModel):
@@ -27,3 +27,14 @@ class ResponseStatusMixin(BaseModel):
         if isinstance(v, str):
             return v.lower() != "fail"
         return v
+
+
+class InstanceIdMixin(BaseModel):
+
+    id_instance: Annotated[
+        int,
+        Field(
+            validation_alias=AliasChoices("idInstance", "id_instance"),
+            description="Account instance identifier, uint64 type, 10 digits",
+        ),
+    ]
--- au_engines/ma_engine/schemas/__init__.py
+++ au_engines/ma_engine/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .account import WaAccountSettingsResponse
 from .auth import AuthRequest, AuthCodeRequest, AuthResponse
 from .contacts import CheckAccountRequest, CheckAccountResponse, GetContactsResponse
+from .credentials import MaChannelCredentialsRequest, MaChannelCredentialsResponse
