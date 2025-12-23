18873a479baa457644a5eb97dd99854d2a4a084a
Andrey Pshenitsyn <include5691@gmail.com>
2025-12-23 15:51:26 +0500

    feat(ma-engine): add enums into root import

--- au_engines/ma_engine/__init__.py
+++ au_engines/ma_engine/__init__.py
@@ -1,5 +1,5 @@
 from .client import MaClient
-from .enums import AuthorizationFailureReason
+from .enums import *
 from .schemas import *
 
 from au_engines.instances_base.enums import InstanceState
